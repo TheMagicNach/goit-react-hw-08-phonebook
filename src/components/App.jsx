dfc317cae1076f5d30cf0cc7ecfae63a063226b9
TheMagicNach <Mrvlados951357@gmail.com>
2023-08-15 16:48:29 +0300

    finish mb

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Model from './Model/Model';
+import Home from './Home/Home';
+import NotFound from './NotFound/NotFound';
+import { useDispatch } from 'react-redux';
+import { refreshUser } from 'redux/auth/operations';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
+import { useAuth } from 'redux/auth/useAuth';
+import Loader from './Loader/Loader';
+import ErrorPage from './ErrorPage/ErrorPage';
+
+const RegisterPage = lazy(() =>
+  import('../pages/RegisterPage')         // ! register-page
+);
+const ContactsPage = lazy(() =>
+  import('../pages/ContactsPage')         // ! contacts-page
+);
+
+const LoginPage = lazy(() =>
+  import('../pages/Login')                // ! login-page
+);
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  const { isRefreshing, error } = useAuth();
+
+  if (error) {
+    return <ErrorPage />;
+  }
+
+  return isRefreshing ? (
+    <Loader />
+  ) : (
+    <div>
+      <Routes>
+        <Route path="/" element={<Model />}>
+          <Route index element={<Home />} />
+
+          <Route
+            path="/register"
+            element={
+              <PublicRoute
+                redirectTo="/contacts"
+                component={<RegisterPage />}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoute redirectTo="/contacts" component={<LoginPage />} />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+            }
+          />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ToastContainer autoClose={2000} />
     </div>
   );
 };
---
+++ src/components/Model/Model.jsx
@@ -0,0 +1,22 @@
+import Header from 'components/Header/Header';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import Loader from 'components/Loader/Loader';
+import { useAuth } from 'redux/auth/useAuth';
+
+const Model = ({ children }) => {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <div>
+      <Header />
+      <div>{children}</div>
+      {!isLoggedIn }
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+};
+
+export default Model;
